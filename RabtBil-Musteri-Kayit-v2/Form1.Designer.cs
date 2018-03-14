8871b75f61d0ca3b9020830e314d8c6010548aad
BySbF <36564237+BySbF@users.noreply.github.com>
2018-03-14 17:53:37 +0300

    Butonlar

--- RabtBil-Musteri-Kayit-v2/Form1.Designer.cs
+++ RabtBil-Musteri-Kayit-v2/Form1.Designer.cs
@@ -28,12 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(77, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(269, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yönetici Girişi";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(77, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(269, 47);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Personel Girişi";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 361);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
