4301fffc3386c07e803ba3d3b3d5869cefe344f0
TempCSharp <36564655+TempCSharp@users.noreply.github.com>
2018-05-12 14:46:42 +0300

    Update Create Database.sql

--- Create Database.sql
+++ Create Database.sql
@@ -9,11 +9,11 @@
 
 CREATE TABLE [dbo].[Kullanicilar] (
     [Id]           INT           IDENTITY (1, 1) NOT NULL,
-    [KullaniciAdi] NVARCHAR (32) NULL,
+    [KullaniciAdi] NVARCHAR (50) NULL,
     [Sifre]        NVARCHAR (32) NULL,
-    [Adi]          NVARCHAR (32) NULL,
-    [Soyadi]       NVARCHAR (32) NULL,
-    [Eposta]       NVARCHAR (32) NULL,
+    [Adi]          NVARCHAR (50) NULL,
+    [Soyadi]       NVARCHAR (50) NULL,
+    [Eposta]       NVARCHAR (50) NULL,
     [Rol]          INT           NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC)
 );
@@ -24,23 +24,23 @@ CREATE TABLE [dbo].[Kullanicilar] (
 
 CREATE TABLE [dbo].[MusteriBilgileri] (
     [Id]               INT            IDENTITY (1, 1) NOT NULL,
-    [FormNo]           NVARCHAR (32)  NULL,
-    [MusteriAdi]       NVARCHAR (32)  NULL,
+    [FormNo]           NVARCHAR (50)  NULL,
+    [MusteriAdi]       NVARCHAR (50)  NULL,
     [Telefon]          NVARCHAR (16)  NULL,
-    [UrunModeli]       NVARCHAR (32)  NULL,
-    [UrunKodlari]      NVARCHAR (32)  NULL,
+    [UrunModeli]       NVARCHAR (50)  NULL,
+    [UrunKodlari]      NVARCHAR (50)  NULL,
     [ArizaTanimi]      NVARCHAR (78)  NULL,
     [Aksesuarlar]      NVARCHAR (156) NULL,
     [EkBilgiler]       NVARCHAR (156) NULL,
     [UrunTakipNo]      NVARCHAR (50)  NULL,
-    [UrunDurumu]       NVARCHAR (32)  NULL,
+    [UrunDurumu]       NVARCHAR (50)  NULL,
     [Ucret]            MONEY          NULL,
     [KaydiYapanID]     INT            NULL,
     [KayitTarihi]      DATETIME       NULL,
     [GuncelleyenID]    INT            NULL,
     [GuncellemeTarihi] DATETIME       NULL,
     [TeslimEdenID]     INT            NULL,
-    [TeslimAlan]       NVARCHAR (32)  NULL,
+    [TeslimAlan]       NVARCHAR (50)  NULL,
     [TeslimTarihi]     DATETIME       NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [KM_Kullaniciler] FOREIGN KEY ([KaydiYapanID]) REFERENCES [dbo].[Kullanicilar] ([Id]) ON DELETE CASCADE ON UPDATE CASCADE
