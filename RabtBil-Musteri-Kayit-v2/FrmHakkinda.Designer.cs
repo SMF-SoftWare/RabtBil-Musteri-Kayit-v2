490499a44d476ea0462de51b9585f198023d149a
TempCSharp <36564655+TempCSharp@users.noreply.github.com>
2018-05-15 21:11:36 +0300

    Update FrmHakkinda

--- RabtBil-Musteri-Kayit-v2/FrmHakkinda.Designer.cs
+++ RabtBil-Musteri-Kayit-v2/FrmHakkinda.Designer.cs
@@ -32,7 +32,7 @@
             this.lblProgramAdi = new System.Windows.Forms.Label();
             this.lblSurum = new System.Windows.Forms.Label();
             this.lblSurumNumarasi = new System.Windows.Forms.Label();
-            this.lblAnaSayfa = new System.Windows.Forms.Label();
+            this.lblAnasayfa = new System.Windows.Forms.Label();
             this.linklblAnaSayfa = new System.Windows.Forms.LinkLabel();
             this.lblLisansDurumu = new System.Windows.Forms.Label();
             this.lblLisans = new System.Windows.Forms.Label();
@@ -42,6 +42,8 @@
             this.lblOgrenci1 = new System.Windows.Forms.Label();
             this.lblCopyRight = new System.Windows.Forms.Label();
             this.picLogo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kapat = new System.Windows.Forms.Label();
             this.gbxSMFSoftWare.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
@@ -49,48 +51,49 @@
             // lblProgramAdi
             // 
             this.lblProgramAdi.AutoSize = true;
-            this.lblProgramAdi.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.lblProgramAdi.Location = new System.Drawing.Point(12, 27);
+            this.lblProgramAdi.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblProgramAdi.Location = new System.Drawing.Point(17, 41);
             this.lblProgramAdi.Name = "lblProgramAdi";
-            this.lblProgramAdi.Size = new System.Drawing.Size(248, 18);
+            this.lblProgramAdi.Size = new System.Drawing.Size(233, 21);
             this.lblProgramAdi.TabIndex = 0;
             this.lblProgramAdi.Text = "RabtBil Müşteri Kayıt Programı";
             // 
             // lblSurum
             // 
             this.lblSurum.AutoSize = true;
-            this.lblSurum.Font = new System.Drawing.Font("Consolas", 9F);
-            this.lblSurum.Location = new System.Drawing.Point(32, 64);
+            this.lblSurum.Location = new System.Drawing.Point(18, 66);
             this.lblSurum.Name = "lblSurum";
-            this.lblSurum.Size = new System.Drawing.Size(49, 14);
+            this.lblSurum.Size = new System.Drawing.Size(60, 21);
             this.lblSurum.TabIndex = 1;
             this.lblSurum.Text = "Sürüm:";
             // 
             // lblSurumNumarasi
             // 
             this.lblSurumNumarasi.AutoSize = true;
-            this.lblSurumNumarasi.Location = new System.Drawing.Point(87, 61);
+            this.lblSurumNumarasi.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSurumNumarasi.Location = new System.Drawing.Point(75, 66);
             this.lblSurumNumarasi.Name = "lblSurumNumarasi";
-            this.lblSurumNumarasi.Size = new System.Drawing.Size(32, 18);
+            this.lblSurumNumarasi.Size = new System.Drawing.Size(32, 21);
             this.lblSurumNumarasi.TabIndex = 2;
             this.lblSurumNumarasi.Text = "2.0";
             // 
-            // lblAnaSayfa
+            // lblAnasayfa
             // 
-            this.lblAnaSayfa.AutoSize = true;
-            this.lblAnaSayfa.Font = new System.Drawing.Font("Consolas", 9F);
-            this.lblAnaSayfa.Location = new System.Drawing.Point(12, 94);
-            this.lblAnaSayfa.Name = "lblAnaSayfa";
-            this.lblAnaSayfa.Size = new System.Drawing.Size(70, 14);
-            this.lblAnaSayfa.TabIndex = 3;
-            this.lblAnaSayfa.Text = "AnaSayfa:";
+            this.lblAnasayfa.AutoSize = true;
+            this.lblAnasayfa.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.lblAnasayfa.Location = new System.Drawing.Point(18, 87);
+            this.lblAnasayfa.Name = "lblAnasayfa";
+            this.lblAnasayfa.Size = new System.Drawing.Size(108, 21);
+            this.lblAnasayfa.TabIndex = 3;
+            this.lblAnasayfa.Text = "İnternet Sitesi:";
             // 
             // linklblAnaSayfa
             // 
             this.linklblAnaSayfa.AutoSize = true;
-            this.linklblAnaSayfa.Location = new System.Drawing.Point(87, 90);
+            this.linklblAnaSayfa.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linklblAnaSayfa.Location = new System.Drawing.Point(123, 87);
             this.linklblAnaSayfa.Name = "linklblAnaSayfa";
-            this.linklblAnaSayfa.Size = new System.Drawing.Size(144, 18);
+            this.linklblAnaSayfa.Size = new System.Drawing.Size(148, 21);
             this.linklblAnaSayfa.TabIndex = 4;
             this.linklblAnaSayfa.TabStop = true;
             this.linklblAnaSayfa.Text = "www.casper.com.tr";
@@ -98,20 +101,20 @@
             // lblLisansDurumu
             // 
             this.lblLisansDurumu.AutoSize = true;
-            this.lblLisansDurumu.Font = new System.Drawing.Font("Consolas", 9F);
-            this.lblLisansDurumu.Location = new System.Drawing.Point(12, 127);
+            this.lblLisansDurumu.Location = new System.Drawing.Point(18, 108);
             this.lblLisansDurumu.Name = "lblLisansDurumu";
-            this.lblLisansDurumu.Size = new System.Drawing.Size(105, 14);
+            this.lblLisansDurumu.Size = new System.Drawing.Size(118, 21);
             this.lblLisansDurumu.TabIndex = 5;
             this.lblLisansDurumu.Text = "Lisans Durumu:";
             // 
             // lblLisans
             // 
             this.lblLisans.AutoSize = true;
+            this.lblLisans.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.lblLisans.ForeColor = System.Drawing.Color.Green;
-            this.lblLisans.Location = new System.Drawing.Point(123, 123);
+            this.lblLisans.Location = new System.Drawing.Point(132, 108);
             this.lblLisans.Name = "lblLisans";
-            this.lblLisans.Size = new System.Drawing.Size(72, 18);
+            this.lblLisans.Size = new System.Drawing.Size(61, 21);
             this.lblLisans.TabIndex = 6;
             this.lblLisans.Text = "Lisanslı";
             // 
@@ -120,9 +123,9 @@
             this.gbxSMFSoftWare.Controls.Add(this.lblOgrenci3);
             this.gbxSMFSoftWare.Controls.Add(this.lblOgrenci2);
             this.gbxSMFSoftWare.Controls.Add(this.lblOgrenci1);
-            this.gbxSMFSoftWare.Location = new System.Drawing.Point(14, 156);
+            this.gbxSMFSoftWare.Location = new System.Drawing.Point(17, 136);
             this.gbxSMFSoftWare.Name = "gbxSMFSoftWare";
-            this.gbxSMFSoftWare.Size = new System.Drawing.Size(311, 131);
+            this.gbxSMFSoftWare.Size = new System.Drawing.Size(350, 109);
             this.gbxSMFSoftWare.TabIndex = 8;
             this.gbxSMFSoftWare.TabStop = false;
             this.gbxSMFSoftWare.Text = "SMF-SoftWare";
@@ -130,67 +133,94 @@
             // lblOgrenci3
             // 
             this.lblOgrenci3.AutoSize = true;
-            this.lblOgrenci3.Location = new System.Drawing.Point(6, 93);
+            this.lblOgrenci3.Location = new System.Drawing.Point(7, 77);
             this.lblOgrenci3.Name = "lblOgrenci3";
-            this.lblOgrenci3.Size = new System.Drawing.Size(256, 18);
+            this.lblOgrenci3.Size = new System.Drawing.Size(247, 21);
             this.lblOgrenci3.TabIndex = 11;
             this.lblOgrenci3.Text = "16300031011 - Selim Baki FINDIK";
             // 
             // lblOgrenci2
             // 
             this.lblOgrenci2.AutoSize = true;
-            this.lblOgrenci2.Location = new System.Drawing.Point(6, 62);
+            this.lblOgrenci2.Location = new System.Drawing.Point(7, 51);
             this.lblOgrenci2.Name = "lblOgrenci2";
-            this.lblOgrenci2.Size = new System.Drawing.Size(224, 18);
+            this.lblOgrenci2.Size = new System.Drawing.Size(232, 21);
             this.lblOgrenci2.TabIndex = 10;
             this.lblOgrenci2.Text = "16300031047 - Fırat GÖRGÜLÜ";
             // 
             // lblOgrenci1
             // 
             this.lblOgrenci1.AutoSize = true;
-            this.lblOgrenci1.Location = new System.Drawing.Point(6, 30);
+            this.lblOgrenci1.Location = new System.Drawing.Point(7, 25);
             this.lblOgrenci1.Name = "lblOgrenci1";
-            this.lblOgrenci1.Size = new System.Drawing.Size(240, 18);
+            this.lblOgrenci1.Size = new System.Drawing.Size(257, 21);
             this.lblOgrenci1.TabIndex = 9;
             this.lblOgrenci1.Text = "16300031031 - Ali KÖMESÖĞÜTLÜ";
             // 
             // lblCopyRight
             // 
             this.lblCopyRight.AutoSize = true;
-            this.lblCopyRight.Location = new System.Drawing.Point(328, 265);
+            this.lblCopyRight.Location = new System.Drawing.Point(369, 224);
             this.lblCopyRight.Name = "lblCopyRight";
-            this.lblCopyRight.Size = new System.Drawing.Size(408, 18);
+            this.lblCopyRight.Size = new System.Drawing.Size(387, 21);
             this.lblCopyRight.TabIndex = 10;
-            this.lblCopyRight.Text = "Copyright © 2018 SMF SoftWare Tüm Hakları Saklıdır";
+            this.lblCopyRight.Text = "Copyright © 2018 SMF SoftWare Tüm Hakları Saklıdır.";
             // 
             // picLogo
             // 
             this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
-            this.picLogo.Location = new System.Drawing.Point(355, 12);
+            this.picLogo.Location = new System.Drawing.Point(377, 41);
             this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(376, 241);
+            this.picLogo.Size = new System.Drawing.Size(380, 177);
             this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picLogo.TabIndex = 9;
             this.picLogo.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(17, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 25);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Hakkında";
+            // 
+            // kapat
+            // 
+            this.kapat.AutoSize = true;
+            this.kapat.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kapat.Location = new System.Drawing.Point(747, 3);
+            this.kapat.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.kapat.Name = "kapat";
+            this.kapat.Size = new System.Drawing.Size(23, 25);
+            this.kapat.TabIndex = 12;
+            this.kapat.Text = "X";
+            this.kapat.Click += new System.EventHandler(this.kapat_Click);
+            // 
             // FrmHakkinda
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(743, 296);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(772, 264);
+            this.Controls.Add(this.kapat);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lblCopyRight);
             this.Controls.Add(this.picLogo);
             this.Controls.Add(this.gbxSMFSoftWare);
             this.Controls.Add(this.lblLisans);
             this.Controls.Add(this.lblLisansDurumu);
             this.Controls.Add(this.linklblAnaSayfa);
-            this.Controls.Add(this.lblAnaSayfa);
+            this.Controls.Add(this.lblAnasayfa);
             this.Controls.Add(this.lblSurumNumarasi);
             this.Controls.Add(this.lblSurum);
             this.Controls.Add(this.lblProgramAdi);
-            this.Font = new System.Drawing.Font("Consolas", 11F);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.MaximizeBox = false;
             this.Name = "FrmHakkinda";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -209,7 +239,7 @@
         private System.Windows.Forms.Label lblProgramAdi;
         private System.Windows.Forms.Label lblSurum;
         private System.Windows.Forms.Label lblSurumNumarasi;
-        private System.Windows.Forms.Label lblAnaSayfa;
+        private System.Windows.Forms.Label lblAnasayfa;
         private System.Windows.Forms.LinkLabel linklblAnaSayfa;
         private System.Windows.Forms.Label lblLisansDurumu;
         private System.Windows.Forms.Label lblLisans;
@@ -219,5 +249,7 @@
         private System.Windows.Forms.Label lblOgrenci1;
         private System.Windows.Forms.PictureBox picLogo;
         private System.Windows.Forms.Label lblCopyRight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label kapat;
     }
 }
--- RabtBil-Musteri-Kayit-v2/FrmHakkinda.cs
+++ RabtBil-Musteri-Kayit-v2/FrmHakkinda.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace RabtBil_Musteri_Kayit_v2
@@ -8,6 +9,20 @@ namespace RabtBil_Musteri_Kayit_v2
         public FrmHakkinda()
         {
             InitializeComponent();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case 0x84:
+                    base.WndProc(ref m);
+                    if ((int)m.Result == 0x1)
+                        m.Result = (IntPtr)0x2;
+                    return;
+            }
+
+            base.WndProc(ref m);
         }
 
         private void FrmHakkinda_Load(object sender, System.EventArgs e)
@@ -24,6 +39,11 @@ namespace RabtBil_Musteri_Kayit_v2
                 lblLisans.Text = "Lisanssız";
                 lblLisans.ForeColor = Color.Red;
             }
+        }
+
+        private void kapat_Click(object sender, System.EventArgs e)
+        {
+            Close();
         }
     }
 }
